0859d36c748cbf1ce2692c59329de2c6ad9492bc
Givem2thekey <48485402+Givem2thekey@users.noreply.github.com>
2022-11-28 15:47:22 +0900

    [Bronze II] Title: 커트라인, Time: 144 ms, Memory: 14564 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+
+		st = new StringTokenizer(br.readLine());
+		int[] arr = new int[N];
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+	    Integer[] tmp = Arrays.stream(arr).boxed().toArray(Integer[]::new);
+		Arrays.sort(tmp,Collections.reverseOrder());
+
+		System.out.println(tmp[k-1]);
+	}
+
+}
