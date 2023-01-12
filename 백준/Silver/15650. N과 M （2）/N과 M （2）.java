b97b3d58124c7e37b1c79bad0e2b25e229e5628e
Givem2thekey <48485402+Givem2thekey@users.noreply.github.com>
2023-01-12 23:53:55 +0900

    [Silver III] Title: N과 M (2), Time: 240 ms, Memory: 18372 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+	static int[] arr;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+
+		arr = new int[M];
+		dfs(N, M, 1, 0);
+
+	}
+
+	public static void dfs(int N, int M, int at, int depth) {
+		if (depth == M) {
+			for (int val : arr) {
+				System.out.print(val + " ");
+			}
+			System.out.println();
+			return;
+		}
+
+		for (int i = at; i <= N; i++) {
+
+			arr[depth] = i;
+			dfs(N, M, i + 1, depth + 1);
+
+		}
+	}
+
+}
