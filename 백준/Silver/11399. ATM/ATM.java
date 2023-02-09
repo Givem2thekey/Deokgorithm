ba6d1ea697166af99a950145e35a7c99058a8df9
Givem2thekey <48485402+Givem2thekey@users.noreply.github.com>
2023-02-09 23:27:18 +0900

    [Silver IV] Title: ATM, Time: 268 ms, Memory: 19424 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		int[] arr =  new int[N];
+
+		for (int i = 0; i < N; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		Arrays.sort(arr);
+		
+		int ans = 0;
+		
+		for (int i = 0; i < arr.length; i++) {
+			ans += arr[i]*N;
+			N--;
+		}
+		
+		System.out.println(ans);
+	}
+
+}
