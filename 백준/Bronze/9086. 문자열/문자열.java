5aaf278109bcb48b1d32452e1ef10e1165e9561b
Givem2thekey <48485402+Givem2thekey@users.noreply.github.com>
2023-02-23 00:01:05 +0900

    [Bronze V] Title: 문자열, Time: 200 ms, Memory: 17552 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
+		int n = sc.nextInt();
+		
+		for (int i = 0; i < n; i++) {
+			String str = sc.next();
+			sb.append(str.charAt(0)).append(str.charAt(str.length()-1)).append("\n");
+		}
+		
+		System.out.println(sb);
+	}
+
+}
