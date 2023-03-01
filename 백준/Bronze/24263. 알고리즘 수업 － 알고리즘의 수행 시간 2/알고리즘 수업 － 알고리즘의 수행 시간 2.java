d0f70f97e9d8cf14dc8e40803753e0fddc460e74
Givem2thekey <48485402+Givem2thekey@users.noreply.github.com>
2023-03-01 22:48:10 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 124 ms, Memory: 14312 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	private void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(br.readLine()+"\n1");
+    }
+	public static void main(String[] args)throws Exception{
+		new Main().solution();
+	}
+
+}
