4dea67da54b8a59a9315381afb69d843218b1574
Givem2thekey <48485402+Givem2thekey@users.noreply.github.com>
2023-03-30 09:55:50 +0900

    [Bronze II] Title: 진법 변환, Time: 212 ms, Memory: 17760 KB -BaekjoonHub

---
+++ 백준/Bronze/2745. 진법 변환/진법 변환.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String str = sc.next();
+		int B = sc.nextInt();
+		int temp = 0;
+
+		System.out.println(Integer.parseInt(str,B));
+
+	}
+
+}
