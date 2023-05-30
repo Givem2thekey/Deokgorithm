1166252489fd85b4d472ebd8925db966862fa4c3
Givem2thekey <48485402+Givem2thekey@users.noreply.github.com>
2023-05-30 23:47:24 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.02 ms, Memory: 77.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int a, int b) {
+        int answer = b/gcd(a,b);
+        while(answer != 1){
+            if(answer % 2 == 0){
+                answer /= 2;
+            }else if(answer % 5 == 0){
+                answer /= 5;
+            }else{
+                return 2;
+            }
+        }
+        return 1;
+    }
+    
+    public int gcd(int a, int b){
+        if(b==0){
+            return a;
+        }else{
+            return gcd(b, a%b);
+        }
+    }
+}
