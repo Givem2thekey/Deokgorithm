b84bb1ba8ca4eefb6871d60c72516cec24d97b4c
Givem2thekey <48485402+Givem2thekey@users.noreply.github.com>
2026-01-25 20:54:44 +0900

    [Bronze V] Title: 꼬마 정민, Time: 100 ms, Memory: 14116 KB -BaekjoonHub

--- 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -1,16 +1,22 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		long a = Long.parseLong(st.nextToken());
+		long b = Long.parseLong(st.nextToken());
+		long c = Long.parseLong(st.nextToken());
 		
-		long A = sc.nextLong();
-		long B = sc.nextLong();
-		long C = sc.nextLong();
+		StringBuilder sb = new StringBuilder();
+		sb.append(a+b+c);
+
+		System.out.print(sb);
 				
-		
-		System.out.println(A+B+C);
+
+
 	}
 
 }
