7b370cbab65be8fd15cb35f629e89284e76d6e25
Givem2thekey <48485402+Givem2thekey@users.noreply.github.com>
2026-01-25 20:12:29 +0900

    [Bronze V] Title: ??!, Time: 108 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.StringTokenizer;
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		String str = st.nextToken();
+		StringBuilder sb = new StringBuilder();
+		sb.append(str).append("??!");
+		System.out.print(sb);
+				
+
+
+	}
+
+}
