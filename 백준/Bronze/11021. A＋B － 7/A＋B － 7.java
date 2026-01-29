9db5d0c3e45025f362225d263042937245271ce2
Givem2thekey <48485402+Givem2thekey@users.noreply.github.com>
2026-01-29 22:56:11 +0900

    [Bronze V] Title: A+B - 7, Time: 124 ms, Memory: 16128 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int T = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+		for(int i=1; i<=T; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			System.out.println("Case #"+i+": "+(a+b));
+		}
+		
+	}
+
+}
