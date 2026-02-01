78b0b6cf3edcbad4683ba9d6756bba89f4fea85c
Givem2thekey <48485402+Givem2thekey@users.noreply.github.com>
2026-02-01 23:11:40 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 112 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -1,21 +1,20 @@
 import java.io.*;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
+
+	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		int i = Integer.parseInt(br.readLine());
-		
-		for(int j=1; j<=i; j++) {
-			for(int k=i-(j-1); k<=i; k++) {
-				bw.write("*");
+		int T = Integer.parseInt(br.readLine());
+		StringBuilder sb=new StringBuilder();
+		for(int i=T; i>0; i--) {
+			for(int j=1;j<=T-i+1; j++) {
+				sb.append("*");
 			}
-			bw.write("\n");
+			sb.append("\n");
 		}
-		bw.flush();
-		bw.close();
-		
+		System.out.print(sb);
 	}
+
 }
