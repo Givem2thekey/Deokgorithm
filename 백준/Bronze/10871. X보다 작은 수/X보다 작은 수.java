1b0f6119af4341f8e1d2651617795d8304d5c463
Givem2thekey <48485402+Givem2thekey@users.noreply.github.com>
2026-02-03 00:42:45 +0900

    [Bronze V] Title: X보다 작은 수, Time: 168 ms, Memory: 17004 KB -BaekjoonHub

---
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb=new StringBuilder();
+		int N = Integer.parseInt(st.nextToken());
+		int X = Integer.parseInt(st.nextToken());
+		
+		int[] arr = new int[N];
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		for(int i=0; i<arr.length;i++) {
+			if(arr[i]<X)sb.append(arr[i]+" ");
+		}
+		System.out.print(sb);
+	}
+
+}
