f3ba03adcec08015a3ef3d1a6108253ed5b17baa
Givem2thekey <48485402+Givem2thekey@users.noreply.github.com>
2026-02-05 07:21:07 +0900

    [Silver V] Title: 너의 평점은, Time: 100 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Silver/25206. 너의 평점은/너의 평점은.java
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.java
@@ -1,48 +1,62 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[][] arr = new String[20][3];
-
-		Map<String, Double> point = new HashMap<String, Double>() {
-			{
-				put("A+", 4.5);
-				put("A0", 4.0);
-				put("B+", 3.5);
-				put("B0", 3.0);
-				put("C+", 2.5);
-				put("C0", 2.0);
-				put("D+", 1.5);
-				put("D0", 1.0);
-				put("F", 0.0);
-				put("P", 0.0);
-			}
-		};
-
-		double sum = 0;
-		double cnt = 0;
-
-		for (int i = 0; i < 20; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < 3; j++) {
-				arr[i][j] = st.nextToken();
-			}
-			
-			if(!arr[i][2].equals("P")) {
-				cnt+=Double.parseDouble(arr[i][1]);
-				sum+=Double.parseDouble(arr[i][1])*point.get(arr[i][2]);
-			}
-
-		}
+		StringBuilder sb = new StringBuilder();
 		
-		System.out.println(sum/cnt);
+		double sum = 0;
+		double gscore = 0;
+		for(int i=0; i<20; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			String subject = st.nextToken();
+			double score = Double.parseDouble(st.nextToken());
+			String grade = st.nextToken();
+			
+			if(!grade.equals("P")) {			
+				sum+=score;;
+				gscore += caculScore(grade, score);
+
+			}
+		}
+		System.out.println(gscore/sum);
+
+		
 	}
 
+	public static double caculScore(String grade,double score) {
+		double gscore=0.0;
+		switch(grade) {
+		case"A+":
+			gscore = 4.5*score;
+			break;
+		case"A0":
+			gscore = 4.0*score;
+			break;
+		case"B+":
+			gscore = 3.5*score;
+			break;
+		case"B0":
+			gscore = 3.0*score;
+			break;
+		case"C+":
+			gscore = 2.5*score;
+			break;
+		case"C0":
+			gscore = 2.0*score;
+			break;
+		case"D+":
+			gscore = 1.5*score;
+			break;
+		case"D0":
+			gscore = 1.0*score;
+			break;
+		case"F":
+			gscore = 0.0*score;
+			break;
+		}
+		return gscore;
+	}
 }
