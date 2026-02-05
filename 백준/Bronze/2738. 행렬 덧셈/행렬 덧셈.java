b1a83d916ebe0297e3101a82f2cb675a968599f1
Givem2thekey <48485402+Givem2thekey@users.noreply.github.com>
2026-02-05 16:59:46 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 144 ms, Memory: 16276 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int[][] arr1 = new int[n][m];
+		int[][] arr2 = new int[n][m];
+		
+		for(int i=0; i<n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j=0; j<m; j++) {
+				arr1[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		for(int i=0; i<n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j=0; j<m; j++) {
+				arr2[i][j] = Integer.parseInt(st.nextToken());
+				sb.append(arr1[i][j]+=arr2[i][j]).append(" ");
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb);
+		
+
+		
+	}
+
+}
