85657a8a595504dbba840cf7f30cb34f0acf4ab7
Givem2thekey <48485402+Givem2thekey@users.noreply.github.com>
2026-02-05 18:28:01 +0900

    [Bronze III] Title: 최댓값, Time: 96 ms, Memory: 14036 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.java
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -1,31 +1,35 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) throws Exception{
+	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
 		
-		int[][] arr = new int[9][9];
-		int max = -1;
-		int maxN = -1;
-		int maxM = -1;
 		
-		for (int i = 0; i < 9; i++) {
+		int[][] arr = new int[9][9];
+		int max = Integer.MIN_VALUE;
+		int x = 0;
+		int y = 0;
+		for(int i=0; i<9; i++) {
 			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < 9; j++) {
+			for(int j=0; j<9; j++) {
 				arr[i][j] = Integer.parseInt(st.nextToken());
 				if(max<arr[i][j]) {
 					max = arr[i][j];
-					maxN = i;
-					maxM = j;
+					x = i;	
+					y = j;
 				}
 			}
 		}
-		System.out.println(max);
-		System.out.println((maxN+1) + " " +(maxM+1));
+		sb.append(max).append("\n");
+		sb.append(x+1).append(" ").append(y+1);
+		System.out.println(sb);
+	
+
+		
 	}
 
 }
