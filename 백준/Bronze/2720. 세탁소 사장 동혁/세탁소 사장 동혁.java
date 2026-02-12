0c2e0fb5b43d0b3f5c97e91a0f299341de04e811
Givem2thekey <48485402+Givem2thekey@users.noreply.github.com>
2026-02-12 19:38:01 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 108 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.java
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.java
@@ -1,30 +1,33 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		int T = sc.nextInt();
-		
-		for (int i = 0; i < T; i++) {
-			int change = sc.nextInt();
-
-			int quarter = 0;
-			int dime = 0;
-			int nickel = 0;
-			int penny = 0;
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int Q = 25;
+		int D = 10;
+		int N = 5;
+		int P = 1;
+		int t = Integer.parseInt(br.readLine());
+		for(int i=0; i<t; i++) {
+			int c = Integer.parseInt(br.readLine());
 			
-			quarter = change/25;
-			change%=25;
-			dime = change/10;
-			change%=10;
-			nickel = change/5;
-			change%=5;
-			penny = change;
+			sb.append(c/Q).append(" ");
+			c%=Q;
+			sb.append(c/D).append(" ");
+			c%=D;
+			sb.append(c/N).append(" ");
+			c%=N;
+			sb.append(c).append("\n");
 			
-			System.out.println(quarter +" "+ dime + " " + nickel + " " + penny);
 		}
+		
+		
+		
+		System.out.println(sb);
+		
 	}
 
 }
