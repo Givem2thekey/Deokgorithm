9595d13833c6bcb7ca22a3a5cdbf27975def4a8a
Givem2thekey <48485402+Givem2thekey@users.noreply.github.com>
2026-02-25 01:38:01 +0900

    [Bronze III] Title: 배수와 약수, Time: 96 ms, Memory: 14080 KB -BaekjoonHub

--- 백준/Bronze/5086. 배수와 약수/배수와 약수.java
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.java
@@ -1,24 +1,36 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
 		
 		while(true) {
-			int a = sc.nextInt();
-			int b = sc.nextInt();
+			st = new StringTokenizer(br.readLine());
+			
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
 			
 			if(a==0&&b==0) {
 				break;
-			}else if(a % b == 0) {
-				System.out.println("multiple");
-			}else if(b % a == 0) {
-				System.out.println("factor");
+			}
+			
+			if(b%a==0) {
+				sb.append("factor").append("\n");
+			}else if(a%b==0) {
+				sb.append("multiple").append("\n");
 			}else {
-				System.out.println("neither");
+				sb.append("neither").append("\n");
 			}
 		}
+		
+				
+		System.out.println(sb);
 	}
 
 }
