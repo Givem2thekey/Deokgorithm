da55ceaff493b9112db2ef6e0422a3a380d63139
Givem2thekey <48485402+Givem2thekey@users.noreply.github.com>
2026-04-28 00:15:07 +0900

    [Bronze I] Title: 약수들의 합, Time: 108 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Bronze/9506. 약수들의 합/약수들의 합.java
+++ 백준/Bronze/9506. 약수들의 합/약수들의 합.java
@@ -1,41 +1,45 @@
-import java.util.LinkedList;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		
+		
 		while(true) {
-			int n = sc.nextInt();
-			if(n == -1) break;
-			LinkedList<Integer> list = new LinkedList<>();
+			String line = br.readLine();
+			if(line == null || line.equals("-1"))break;
 			
+			int n = Integer.parseInt(line);
+			
+			ArrayList<Integer> al = new ArrayList<>();
 			int sum = 0;
 			
-			for (int i = 1; i < n; i++) {
-				if(n%i == 0) {
-					list.add(i);
-					sum+=i;
-
+			for(int i=1; i<=n/2; i++) {
+				if(n%i==0) {
+					al.add(i);
+					sum +=i;
 				}
-				
 			}
-
+			
 			if(sum == n) {
 				sb.append(n).append(" = ");
-				for (int i = 0; i < list.size()-1; i++) {
-					sb.append(list.get(i)).append(" + ");
+				for(int i=0; i<al.size(); i++) {
+					sb.append(al.get(i));
+					if(i<al.size()-1)sb.append(" + ");
 				}
-				sb.append(list.getLast()).append("\n");
+				sb.append("\n");
 			}else {
-				sb.append(n).append(" is NOT perfect.").append("\n");
+				sb.append(n).append(" is NOT perfect.\n");
 			}
-	
 		}
-		
-		System.out.println(sb);
+	
+		System.out.print(sb);
 	}
 
 }
